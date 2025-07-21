fc90036446551bfb7f9f9c99fcf2f383b133f632
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2025-07-21 09:33:20 +0200

    Create icons.blade.php

---
+++ resources/views/icons.blade.php
@@ -0,0 +1,9 @@
+<div class="flex flex-wrap gap-2">
+    <a href="https://moox.heco.si/icons" target="_blank">
+        <i class="si si-moox text-2xl"></i>
+    </a>
+</div>
+
+And maybe ...
+
+https://moox.heco.si/components
