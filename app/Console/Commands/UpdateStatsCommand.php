f579670eece0aca3774268be7423b74a42f47b4e
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2025-08-31 11:06:24 +0200

    Update UpdateStatsCommand.php

--- app/Console/Commands/UpdateStatsCommand.php
+++ app/Console/Commands/UpdateStatsCommand.php
@@ -15,14 +15,24 @@ class UpdateStatsCommand extends Command
     {
         $this->info('Dispatching stats update job...');
 
+        \Log::info('UpdateStatsCommand: Starting command execution');
+
         try {
+            \Log::info('UpdateStatsCommand: About to dispatch job');
             UpdateStatsJob::dispatch();
+            \Log::info('UpdateStatsCommand: Job dispatched successfully');
+
             $this->info('Stats update job has been queued successfully!');
 
             $this->info('You can check the queue status with: php artisan queue:work --once');
             $this->info('Or check logs in storage/logs/laravel.log');
 
         } catch (\Exception $e) {
+            \Log::error('UpdateStatsCommand: Failed to dispatch job', [
+                'error' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+            ]);
+
             $this->error('Failed to dispatch job: '.$e->getMessage());
 
             return Command::FAILURE;
